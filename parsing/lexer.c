01d10f0375aa6e198c448c373c9170e20905d8bb
vlrlmn <lomakina.valerie@gmail.com>
2024-06-06 19:23:53 +0400

    added lexer

--- parsing/lexer.c
+++ parsing/lexer.c
@@ -6,27 +6,35 @@
 /*   By: lomakinavaleria <lomakinavaleria@studen    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/06 17:36:36 by lomakinaval       #+#    #+#             */
-/*   Updated: 2024/06/06 17:58:44 by lomakinaval      ###   ########.fr       */
+/*   Updated: 2024/06/06 19:22:40 by lomakinaval      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void redir_lexer(t_cmd *cmd, t_args *args)
+{
+    t_redir *redircmd;
+
+    redircmd = (t_redir *)cmd;
+    if ()
+    lexical_analysis(redircmd->cmd, args);
+
+
+}
+
 void    lexical_analysis(t_cmd *cmd, t_args *args)
 {
     t_pipe *pipecmd;
-    t_redir *redircmd;
     
     if(cmd->type == PIPE)
     {
-        pipecmd = (t_pipe *)cmd;
+    pipecmd = (t_pipe *)cmd;
         lexical_analysis(pipecmd->left, args);
         lexical_analysis(pipecmd->right, args);
     }
     else if (cmd->type == REDIR)
     {
-        redircmd = (t_redir *)cmd;
-        lexical_analysis(redircmd->cmd, args);
     }
     else if (cmd->type == EXEC)
         exec_lexer(cmd, args);
