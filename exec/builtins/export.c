f02f5cecda09e3381f14c64dc313a5958732692c
Sofya <abdulkina.s328@gmail.com>
2024-07-08 15:51:42 +0400

    freed leaks in cd

--- exec/builtins/export.c
+++ exec/builtins/export.c
@@ -6,7 +6,7 @@
 /*   By: sabdulki <sabdulki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/23 17:48:27 by lomakinaval       #+#    #+#             */
-/*   Updated: 2024/07/05 15:46:49 by sabdulki         ###   ########.fr       */
+/*   Updated: 2024/07/08 15:32:28 by sabdulki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -158,7 +158,7 @@ char    *get_str_before_sign(char *str, char sign)
     while (str[i] != sign && i < str_len)
         i++;
     len = i;
-    result = malloc(sizeof(char) * (len + 1));
+    result = malloc(sizeof(char) * (len + 1)); //alloc 1
     if (!result)
         return (perror("malloc"), NULL);
     ft_strlcpy(result, str, len + 1);
