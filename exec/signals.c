e9795cb1db8c027c8aac10b0021a04f65dc735f6
Sofya Abdulkina <sabdulki@lab1r3s10.42abudhabi.ae>
2024-07-09 15:33:42 +0400

    stashed

--- exec/signals.c
+++ exec/signals.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lomakinavaleria <lomakinavaleria@studen    +#+  +:+       +#+        */
+/*   By: sabdulki <sabdulki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 15:39:45 by sabdulki          #+#    #+#             */
-/*   Updated: 2024/07/09 11:49:53 by lomakinaval      ###   ########.fr       */
+/*   Updated: 2024/07/09 15:31:54 by sabdulki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,18 +44,8 @@ void	handle_sigint(int sig)
 			set_status(STOP_HEREDOC);
 			g_exit_status = 1; //or 130
 		}
-		if (get_status(interrupted) == 0) { // Shell is waiting for input
-            printf("\n"); // Print a newline to move to the next line
-            rl_on_new_line(); // Prepare readline to start a new line
-            // rl_replace_line("", 0); // Clear the current input line
-            rl_redisplay(); // Redisplay the prompt
-            interrupted = 1; // Set the interruption flag
-        } else { // Shell is executing a command
-            g_exit_status = 130; // Set the exit status to 130
-            interrupted = 0; // Reset the interruption flag
-        }
 		// rl_replace_line("", 0);
-		// write(STDERR_FILENO, "\n", 1);
-		// write_new_promt();
+		write(STDERR_FILENO, "\n", 1);
+		write_new_promt();
 	}
 }
